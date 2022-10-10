cfdd9539f64afcb2c5ac9ee462238012b5e9fedd
chenyongpeng <947796393@qq.com>
2022-10-10 17:25:18 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    App
+    <!-- 1. 使用根模块state的数据   -->
+    <p>{{$store.state.username}}</p>
+    <!-- 2. 使用根模块getters的数据   -->
+    <p>{{$store.getters['newName']}}</p>
+    <button @click="mutationsFn">mutationsFn</button>
+    <hr />
+    <ul>
+      <li v-for="item in $store.getters.boys" :key="item.id">{{item.name}}</li>
+    </ul>
+    <hr />
+    <!-- 使用模块A的username -->
+    <p>A的username --- {{$store.state.a.username}}</p>
+    <p>A的changeName --- {{$store.getters.changeName}}</p>
+    <hr />
+    <p>B的username --- {{$store.state.b.username}}</p>
+    <p>B的changeName --- {{$store.getters['b/changeName']}}</p>
+    <button @click="$store.commit('b/update')">修改username</button>
+    <button @click="$store.dispatch('b/update')">异步修改username</button>
+    <hr />
+    <!-- 修改数据，测试是否持久化 -->
+    App {{$store.state.user.profile.account}}
+    <button @click="$store.commit('user/setUser',{account:'zhousg'})">设置用户信息</button>
+  </div>
 </template>
-
+<script setup>
+import { useStore } from 'vuex'
+const store = useStore()
+const mutationsFn = () => {
+  store.dispatch('updateName')
+}
+</script>
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,98 @@
 import { createStore } from 'vuex'
+import createPersistedstate from 'vuex-persistedstate'
 
-export default createStore({
-  state: {
+import user from './modules/user'
+import cart from './modules/cart'
+import category from './modules/category'
+
+const moduleA = {
+  // 子模块state建议写成函数
+  state: () => {
+    return {
+      username: '模块A'
+    }
   },
   getters: {
+    changeName (state) {
+      return state.username + 'AAAAAAAAA'
+    }
+  }
+}
+const moduleB = {
+  // 带命名空间的模块
+  namespaced: true,
+  state: () => {
+    return {
+      username: '模块B'
+    }
+  },
+  getters: {
+    changeName (state) {
+      return state.username + 'BBBBBBB'
+    }
   },
   mutations: {
+    // 修改名字的mutation
+    update (state) {
+      state.username = 'BBBBBB' + state.username
+    }
   },
   actions: {
+    update ({ commit }) {
+      // 假设请求
+      setTimeout(() => {
+        commit('update')
+      }, 2000)
+    }
+  }
+}
+// 创建vuex仓库并导出
+export default createStore({
+  state: {
+    // 数据
+    username: 'zs',
+    person: [
+      { id: 1, name: 'tom', gender: '男' },
+      { id: 2, name: 'lucy', gender: '女' },
+      { id: 3, name: 'jack', gender: '男' }
+    ]
+  },
+  getters: {
+    // vuex的计算属性
+    newName (state) {
+      return state.username + '!!!'
+    },
+    boys: (state) => {
+      return state.person.filter(p => p.gender === '男')
+    }
+  },
+  mutations: {
+    // 改数据函数
+    updateName (state) {
+      state.username = 'ls'
+    }
+  },
+  actions: {
+    // 请求数据函数
+    updateName (ctx) {
+      // 发请求
+      setTimeout(() => {
+        ctx.commit('updateName')
+      }, 1000)
+    }
   },
   modules: {
-  }
+    // 分模块
+    a: moduleA,
+    b: moduleB,
+    user,
+    cart,
+    category
+  },
+  plugins: [
+    createPersistedstate({
+      key: 'erabbit-client-pc-store',
+      paths: ['user', 'cart', 'category']
+    })
+  ]
 })
